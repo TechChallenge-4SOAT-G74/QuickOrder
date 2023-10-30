e5c33f9ca276b10c0d1950650a6bbe562378c16c
FranciscoThadeu <fran_thadeu@hotmail.com>
2023-10-30 19:19:48 -0300

    Consultar pedido

--- src/Core/Application/Dtos/PedidoDto.cs
+++ src/Core/Application/Dtos/PedidoDto.cs
@@ -1,9 +1,4 @@
 ﻿using QuickOrder.Core.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuickOrder.Core.Application.Dtos
 {
@@ -17,5 +12,6 @@ namespace QuickOrder.Core.Application.Dtos
         public double ValorPedido { get; set; }
         public string? Observacao { get; set; }
         public bool PedidoPago { get; set; }
+        public string? StatusPedido { get; set; }
     }
 }
